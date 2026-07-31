4f603692cc004181c2ff6b2460f2244291b66f55
gerashisch <gerashisch@gmail.com>
2026-07-31 23:36:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello");
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function(){
+
+let value = searchInput.value.toLowerCase();
+
+let sections = document.querySelectorAll("section");
+
+sections.forEach(function(section){
+
+let text = section.innerText.toLowerCase();
+
+if(text.includes(value)){
+section.style.display = "block";
+}
+else{
+section.style.display = "none";
+}
+
+});
+
+});
